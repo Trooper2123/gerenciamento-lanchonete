43ffa81fbee367dc1a71fc9cf0f1bc3c240be35a
T_Trooper <tete.leticia@gmail.com>
2023-06-06 20:23:11 -0300

    criação do projeto e configurações básicas de DB e Swagger

---
+++ src/main/java/com/trooper/gerenciamentoredelanchonetes/GerenciamentoRedeLanchonetesApplication.java
@@ -0,0 +1,13 @@
+package com.trooper.gerenciamentoredelanchonetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoRedeLanchonetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoRedeLanchonetesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trooper/gerenciamentoredelanchonetes/GerenciamentoRedeLanchonetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trooper.gerenciamentoredelanchonetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoRedeLanchonetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
